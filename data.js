88d36964fb8422963f049803246d9af6bf9d41bf
asmckeithan <annesha.mckeithan@gmail.com>
2024-04-17 21:11:08 -0400

    Part 1

---
+++ data.js
@@ -0,0 +1,8 @@
+console.log('Data Collection')
+
+console.log(`Refactoring the Old Code`)
+let string = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
+let data = " "
+let array = string.split ('\n')
+console.log(array)
+
